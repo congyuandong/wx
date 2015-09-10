f015a79b713bcfbfd7932689ce77c722e64709b1
yuandong.cyd <yuandong.cyd@alibaba-inc.com>
2015-09-10 23:17:28 +0800

    add console

---
+++ console.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# node      Start up node server daemon
+#
+# chkconfig: 345 85 15
+# description: Forever for Node.js
+#
+DEAMON=/root/www/wx/app.js
+LOG=/root/www/wx/log
+PID=/tmp/forever.pid
+
+case "$1" in
+    start)
+        forever start -l $LOG/forever.log -o $LOG/forever_out.log -e $LOG/forever_err.log --pidFile $PID -a $DEAMON
+        ;;
+    stop)
+        forever stop --pidFile $PID $DEAMON
+        ;;
+    stopall)
+        forever stopall --pidFile $PID
+        ;;
+    restartall)
+        forever restartall --pidFile $PID
+        ;;
+    reload|restart)
+        forever restart -l $LOG/forever.log -o $LOG/forever_out.log -e $LOG/forever_err.log --pidFile $PID -a $DEAMON
+        ;;
+    list)
+        forever list
+        ;;
+    *)
+        echo "Usage: /etc.init.d/node {start|stop|restart|reload|stopall|restartall|list}"
+        exit 1
+        ;;
+esac
+exit 0
